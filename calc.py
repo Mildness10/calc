550ebbb0501b26fceabb99e8e2c48296ee0fcf30
Mildness10 <m.akomoize@gmail.com>
2022-12-19 19:57:06 -0500

    created the functions to handle the respective operations (add, sub, mul, div)

--- calc.py
+++ calc.py
@@ -21,14 +21,40 @@ def clear():
 def add():
     first_no = e.get()
     global num1
+    global operator
+    operator = 'addition'
     num1 = int(first_no)
     e.delete(0, END)
-    return
+
+def subtract():
+    first_no = e.get()
+    global num1
+    global operator
+    operator = 'subtraction'
+    num1 = int(first_no)
+    e.delete(0, END)
+
+def multiply():
+    first_no = e.get()
+    global num1
+    global operator
+    operator = 'multiplication'
+    num1 = int(first_no)
+    e.delete(0, END)
+
+def divide():
+    first_no = e.get()
+    global num1
+    global operator
+    operator = 'division'
+    num1 = int(first_no)
+    e.delete(0, END)
 
 def equal():
     second_no = e.get()
     e.delete(0, END)
     e.insert(0, num1 + int(second_no))
+
 
 #row_one
 no_1 = Button(root, text="1", padx=40, pady=20, command=lambda: click(1)).grid(row=3,column=0)
@@ -39,17 +65,17 @@ add_but = Button(root, text="+", padx=40, pady=20, command=add).grid(row=3,colum
 no_4 = Button(root, text="4", padx=40, pady=20, command=lambda: click(4)).grid(row=2,column=0)
 no_5 = Button(root, text="5", padx=40, pady=20, command=lambda: click(5)).grid(row=2,column=1)
 no_6 = Button(root, text="6", padx=40, pady=20, command=lambda: click(6)).grid(row=2,column=2)
-sub_but = Button(root, text="-", padx=40, pady=20).grid(row=2,column=3)
+sub_but = Button(root, text="-", padx=40, pady=20, command=subtract).grid(row=2,column=3)
 #row_three
 no_7 = Button(root, text="7", padx=40, pady=20, command=lambda: click(7)).grid(row=1,column=0)
 no_8 = Button(root, text="8", padx=40, pady=20, command=lambda: click(8)).grid(row=1,column=1)
 no_9 = Button(root, text="9", padx=40, pady=20, command=lambda: click(9)).grid(row=1,column=2)
-mul_but = Button(root, text="*", padx=40, pady=20).grid(row=1,column=3)
+mul_but = Button(root, text="*", padx=40, pady=20, command=multiply).grid(row=1,column=3)
 #row_four
 no_0 = Button(root, text="0", padx=40, pady=20, command=lambda: click(0)).grid(row=4,column=0)
 clear_but = Button(root, text='C', padx=39, pady=20, command=clear).grid(row=4, column=1)
 equal_but = Button(root, text="=", padx=39, pady=20, command=equal).grid(row=4,column=2)
-div_but = Button(root, text="/", padx=40, pady=20).grid(row=4,column=3)
+div_but = Button(root, text="/", padx=40, pady=20, command=divide).grid(row=4,column=3)
 
 
 
