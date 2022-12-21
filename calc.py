3e4dc99a6c0cae1ae6bbb16d35e4a8b1273e5909
Mildness10 <m.akomoize@gmail.com>
2022-12-20 22:25:45 -0500

    corrected an error in the operator functions

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 root = Tk()
 root.title("Calculator")
@@ -53,24 +54,33 @@ def divide():
 def equal():
     second_no = e.get()
     e.delete(0, END)
-    e.insert(0, num1 + int(second_no))
+    
+    if operator == "addition":
+        e.insert(0, num1 + int(second_no))
+    if operator == "subtraction":
+        e.insert(0, num1 - int(second_no))
+    if operator == "multiplication":
+        e.insert(0, num1 * int(second_no))
+    if operator == "division":
+        e.insert(0, num1 / int(second_no))
+    
 
-
+# 
 #row_one
-no_1 = Button(root, text="1", padx=40, pady=20, command=lambda: click(1)).grid(row=3,column=0)
-no_2 = Button(root, text="2", padx=40, pady=20, command=lambda: click(2)).grid(row=3,column=1)
-no_3 = Button(root, text="3", padx=40, pady=20, command=lambda: click(3)).grid(row=3,column=2)
-add_but = Button(root, text="+", padx=40, pady=20, command=add).grid(row=3,column=3)
+no_7 = Button(root, text="7", padx=40, pady=20, command=lambda: click(7)).grid(row=1,column=0)
+no_8 = Button(root, text="8", padx=40, pady=20, command=lambda: click(8)).grid(row=1,column=1)
+no_9 = Button(root, text="9", padx=40, pady=20, command=lambda: click(9)).grid(row=1,column=2)
+mul_but = Button(root, text="*", padx=40, pady=20, command=multiply).grid(row=1,column=3)
 #row_two
 no_4 = Button(root, text="4", padx=40, pady=20, command=lambda: click(4)).grid(row=2,column=0)
 no_5 = Button(root, text="5", padx=40, pady=20, command=lambda: click(5)).grid(row=2,column=1)
 no_6 = Button(root, text="6", padx=40, pady=20, command=lambda: click(6)).grid(row=2,column=2)
 sub_but = Button(root, text="-", padx=40, pady=20, command=subtract).grid(row=2,column=3)
 #row_three
-no_7 = Button(root, text="7", padx=40, pady=20, command=lambda: click(7)).grid(row=1,column=0)
-no_8 = Button(root, text="8", padx=40, pady=20, command=lambda: click(8)).grid(row=1,column=1)
-no_9 = Button(root, text="9", padx=40, pady=20, command=lambda: click(9)).grid(row=1,column=2)
-mul_but = Button(root, text="*", padx=40, pady=20, command=multiply).grid(row=1,column=3)
+no_1 = Button(root, text="1", padx=40, pady=20, command=lambda: click(1)).grid(row=3,column=0)
+no_2 = Button(root, text="2", padx=40, pady=20, command=lambda: click(2)).grid(row=3,column=1)
+no_3 = Button(root, text="3", padx=40, pady=20, command=lambda: click(3)).grid(row=3,column=2)
+add_but = Button(root, text="+", padx=40, pady=20, command=add).grid(row=3,column=3)
 #row_four
 no_0 = Button(root, text="0", padx=40, pady=20, command=lambda: click(0)).grid(row=4,column=0)
 clear_but = Button(root, text='C', padx=39, pady=20, command=clear).grid(row=4, column=1)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
-e.pack()
+square_but = Button(root, text="x^2", padx=40, pady=20, command=lambda: click(1)).grid(row=3,column=0)
+# no_2 = Button(root, text="2", padx=40, pady=20, command=lambda: click(2)).grid(row=3,column=1)
+# no_3 = Button(root, text="3", padx=40, pady=20, command=lambda: click(3)).grid(row=3,column=2)
+# add_but = Button(root, text="+", padx=40, pady=20, command=add).grid(row=3,column=3)
